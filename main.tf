bc52322e55796d1a63633c64e2c80a36ddf6b0d4
darshan49 <42926639+darshan49@users.noreply.github.com>
2019-08-30 16:06:36 +0530

    testing webhooks

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
+#sample testing for webhook
 #create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "HelloWorld"
+# t2.micro node with an AWS Tag naming it "HelloWorld" 
 provider "aws" {
   region = "us-west-2"
 }
